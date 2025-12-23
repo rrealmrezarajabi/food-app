cad71241a5fb3c850d0e485e52d13f87f7e18da1
rrealmrezarajabi <rrealmrezarajabi@gmail.com>
2025-12-23 13:08:46 +0330

    navbar added and changed project background

---
+++ src/app/meals/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      meals page
+    </div>
+  )
+}
+
+export default page
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="w-full bg-zinc-900 text-zinc-100 shadow-lg px-6 py-4 flex items-center justify-between">
+      <div className="text-2xl font-bold text-emerald-400">Food App</div>
+
+      <div className="flex gap-6 font-medium">
+        <Link
+          href="/meals"
+          className="text-zinc-300 hover:text-emerald-400 transition-colors"
+        >
+          Explore Meals
+        </Link>
+
+        <Link
+          href="/community"
+          className="text-zinc-300 hover:text-emerald-400 transition-colors"
+        >
+          Join Community
+        </Link>
+      </div>
+    </nav>
+  );
+}
