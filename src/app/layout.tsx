104bf775fc8460dbfbc23c4b0371a7b0c9cd05df
rrealmrezarajabi <rrealmrezarajabi@gmail.com>
2025-12-25 12:33:30 +0330

    context added with add to cart function

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/NavBar";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Food App",
@@ -25,16 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`
-          ${geistSans.variable} 
-          ${geistMono.variable} 
-          antialiased 
-          bg-zinc-950 
-          text-zinc-100 
-          min-h-screen
-        `}
-      >
+      <body className=" bg-zinc-950 text-zinc-100 min-h-screen">
         <Navbar />
         <main className="px-6 py-4">{children}</main>
       </body>
---
+++ src/context/CartContext.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { createContext, useContext, useState } from "react";
+import { Meal } from "@/lib/types/meals";
+
+interface cartItem extends Meal{
+    qty:number
+}
+
+type CartContextType = {
+  cart: cartItem[];
+  addToCart: (meal: Meal) => void;
+};
+
+const CartContext = createContext<CartContextType | null>(null);
+
+export function cartProvider({ children }: { children: React.ReactNode }) {
+  const [cart, setCart] = useState<cartItem[]>([]);
+
+  const addToCart = (meal:Meal) => {
+    const exist = cart.find((item)=> item.id === meal.id)
+    if(exist){
+        setCart(
+            cart.map((item)=> item.id === meal.id ? {...item , qty:item.qty + 1 } : item)
+        )
+    }
+    else{
+        setCart([...cart , {...meal , qty:1}])
+    }
+  };
+
+  return (
+    <CartContext.Provider value={{ cart, addToCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+}
+export function useCart() {
+  const context = useContext(CartContext);
+  if (!context) {
+    throw new Error("useCart must be used inside CartProvider");
+  }
+  return context;
+}
